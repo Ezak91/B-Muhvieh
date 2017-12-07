13f37e5420b7b0bfadab53fe10d828f1ff8fcd92
Marc Szymkowiak <marc.szymkowiak91@googlemail.com>
2017-12-07 14:27:19 +0100

    add navigation bar

--- includes/main.php
+++ includes/main.php
@@ -7,6 +7,8 @@
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/navbar.css">
     <!-- Javascript -->
     <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
@@ -15,6 +17,7 @@
   </head>
   <body>
     <div>
+       <?php include("navbar.php"); ?>
        <?php include("message.php"); ?>
        <?php include($_POST['inc']); ?>
     </div>
---
+++ includes/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar-default">
+ <div class="container-fluid">
+   <div class="navbar-header">
+     <a class="navbar-brand" href="#">B-Muhvieh</a>
+   </div>
+   <ul class="nav navbar-nav">
+     <li><a href="index.php?inc=movies.php">My Movies</a></li>
+     <li><a href="index.php?inc=watchlist.php">Watchlist</a></li>
+   </ul>
+   <ul class="nav navbar-nav navbar-right">
+       <?php
+       if(isset($_SESSION["userid"])) {
+         echo"<li><a id='logout-nav-link' href='logout.php'><span class='glyphicon glyphicon-log-in'></span> Logout</a></li>";
+       }
+       else
+       {
+         echo"<li><a id='login-nav-link' href='#'><span class='glyphicon glyphicon-log-in'></span> Login</a></li>";
+         echo"<li><a id='register-nav-link' href='#'><span class='glyphicon glyphicon-user'></span> Sign Up</a></li>\r\n";
+       }
+       ?>
+  </ul>
+ </div>
+</nav>

fb9b34cad36e5c0e2bdea0a6a284b90bd4c7103a
Marc Szymkowiak <marc.szymkowiak91@googlemail.com>
2018-01-09 15:55:28 +0100

    show movieinformation (first commit)

--- includes/main.php
+++ includes/main.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/circle.css">
     <!-- Javascript -->
     <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
@@ -17,6 +18,7 @@
     <script type='text/javascript' src="js/pure.js"></script>
     <script type='text/javascript' src="js/search.js"></script>
     <script type='text/javascript' src="js/loadmovies.js"></script>
+    <script type='text/javascript' src="js/loadMovieInfo.js"></script>
   </head>
   <body>
     <div>
--- includes/movieinfo.php
+++ includes/movieinfo.php
@@ -1,3 +1,12 @@
 <?php
-echo "Filminfo";
+if(!isset($_SESSION["userid"])) {
+  echo "You are not allowed to see this";
+  exit;
+}
+else {
+  include("movieinfo.html");
+}
 ?>
+<script type="text/javascript">
+    loadMovieInfo(<?php echo$_GET["tmdbid"];?>);
+</script>

ed9e0a66e83b08eb5b01347fbd99a70b65f808fa
Marc Szymkowiak <marc.szymkowiak91@googlemail.com>
2018-03-28 18:51:13 +0200

    order movies by title asc

--- includes/loadmovie.php
+++ includes/loadmovie.php
@@ -3,10 +3,10 @@ include "../conf/conf_db.php";
 $userid = $_POST["userid"];
 $list = $_POST["list"];
 if($list == "user") {
-  $query = $mysqli->query("SELECT * from user_movie JOIN movie ON user_movie.tmdb_id = movie.tmdb_id where user_movie.user_id = $userid");
+  $query = $mysqli->query("SELECT * from user_movie JOIN movie ON user_movie.tmdb_id = movie.tmdb_id where user_movie.user_id = $userid ORDER BY title ASC");
 }
 if($list == "watch") {
-  $query = $mysqli->query("SELECT * from watch JOIN movie ON watch.tmdb_id = movie.tmdb_id where watch.user_id = $userid");
+  $query = $mysqli->query("SELECT * from watch JOIN movie ON watch.tmdb_id = movie.tmdb_id where watch.user_id = $userid ORDER BY title ASC");
 }
 $rows = array();
 while($result = mysqli_fetch_assoc($query)) {
